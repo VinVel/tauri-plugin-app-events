939128f4ecb04a209ac8676c461469a6a26d01c2
VinVel <git@velcore.net>
2026-08-05 20:59:34 +0200

    refactor: remove duplicate iOS lifecycle logging

--- ios/Sources/AppEventsPlugin.swift
+++ ios/Sources/AppEventsPlugin.swift
@@ -4,13 +4,10 @@
  * SPDX-License-Identifier: MIT
  */
 
-import OSLog
 import SwiftRs
 import Tauri
 import UIKit
 import WebKit
-
-private let log = OSLog(subsystem: "tauri-plugin-app-events", category: "plugin")
 
 private final class SetEventHandlerArgs: Decodable {
   let handler: Channel
@@ -70,7 +67,6 @@ final class AppEventsPlugin: Plugin, UIGestureRecognizerDelegate {
 
   @objc
   private func applicationDidBecomeActive(notification: NSNotification) {
-    os_log(.debug, log: log, "Application did become active")
     let event = JSObject()
     trigger("resume", data: event)
     resumeChannel?.send(event)
@@ -78,7 +74,6 @@ final class AppEventsPlugin: Plugin, UIGestureRecognizerDelegate {
 
   @objc
   private func applicationDidEnterBackground(notification: NSNotification) {
-    os_log(.debug, log: log, "Application did enter background")
     let event = JSObject()
     trigger("pause", data: event)
     pauseChannel?.send(event)
@@ -90,21 +85,18 @@ final class AppEventsPlugin: Plugin, UIGestureRecognizerDelegate {
       return
     }
 
-    os_log(.debug, log: log, "Back gesture")
     var event = JSObject()
     event["canGoBack"] = webview?.canGoBack ?? false
     trigger("back-key-down", data: event)
   }
 
   @objc public func setResumeHandler(_ invoke: Invoke) throws {
-    os_log(.debug, log: log, "setResumeHandler")
     let args = try invoke.parseArgs(SetEventHandlerArgs.self)
     resumeChannel = args.handler
     invoke.resolve()
   }
 
   @objc public func setPauseHandler(_ invoke: Invoke) throws {
-    os_log(.debug, log: log, "setPauseHandler")
     let args = try invoke.parseArgs(SetEventHandlerArgs.self)
     pauseChannel = args.handler
     invoke.resolve()
